cc0a0670f04391f9a8c5a864a0e5a6767d3fda21
Harsh Jaiswal <harshjais369@gmail.com>
2023-10-21 23:27:09 +0530

    Added PrimeNumberGenerator

---
+++ PrimeNumberGenerator.java
@@ -0,0 +1,23 @@
+public class PrimeNumberGenerator {
+    public static void generatePrimes(int limit) {
+        boolean[] isPrime = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++) {
+            isPrime[i] = true;
+        }
+        
+        for (int p = 2; p * p <= limit; p++) {
+            if (isPrime[p]) {
+                for (int i = p * p; i <= limit; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+        
+        System.out.println("Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
