dee5d649714c19642b9f2805a0ecfa6234cd610e
suryapsp <techie-human@protonmail.com>
2023-10-27 09:55:00 +0530

    added Triangle star pattern

---
+++ trianglePattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for(row = 1; row <= 5; row++){
+        for(column = 0; column < row; column++){
+            cout<<row;
+        }
+        cout<<endl;
+    }
+}
